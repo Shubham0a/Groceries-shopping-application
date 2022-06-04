f6e58b2c55e47717e6acd691aff64dfc990f0b13
Shubham kumar <75657176+Shubham-0a@users.noreply.github.com>
2022-06-05 04:12:06 +0530

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,31 +1,3 @@
-// alert("Welcome to Aapnocart ---Shubham kumar");
-// var viewBtm1 = document.querySelector("#viewbtndairy");
-// var viewBtm2 = document.querySelector("#viewbtnsnacks");
-// var viewBtm3 = document.querySelector("#viewbtnvegetable");
-
-// function setNormalcolor:() {
-// 	viewBtm1.style.Background = "yellow";
-// }
-// function setHovercolor() {
-// 	viewBtm1.style.Background = "red";
-// }
-
-// var viewBtm1=[milk,cream,butter];
-// var viewBtm2=[biscuit,mixture,chips];
-// var viewBtm3=[carrot,potato,pazhampuri];
-
-// viewBtm1.addEventlistener("mouseover", setHovercolor);
-// viewBtm1.addEventlistener("mouseout", setNormalcolor);
-// viewBtm1.addEventlistener("click" printMore);
-
-// function printMore()
-// {
-// 	if(userInput.value=="Monday");
-// 	{
-// 		for (var i = 0; i >= 0; i--) {
-// 			var parabox;
-// var answer=prompt("Enter your name.");
-// alert("welcome to Aapnocart!! mr./mrs. " + answer + ". Have a nice day....");
 var answer=prompt("Enter your name.");
 if(answer == "")
 {
